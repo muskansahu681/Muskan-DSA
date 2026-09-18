1d88efa6fef9df8e7ae8b07fcb88d931461911a6
Muskan <151606349+muskansahu681@users.noreply.github.com>
2026-09-18 09:44:55 +0530

    Time: 3 ms (33.87%), Space: 10.9 MB (36.38%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string> words;
+        string word = "";
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] != ' '){
+                word += s[i] ;
+            }
+            else if (word != ""){
+                    words.push_back(word);
+                    word = "";
+                }
+            }
+            if(word != ""){
+                words.push_back(word);
+            }
+            string ans = "";
+            for(int i = words.size() - 1 ; i >= 0; i--){
+            ans += words[i];
+
+            if(i > 0 ){
+                ans += " ";
+            }
+        }
+        return ans;
+    }
+};
